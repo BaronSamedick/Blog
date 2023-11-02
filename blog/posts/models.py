79009e513854c24b293e6303a0b263979e3fa2a6
ds.eruslanov <ds.eruslanov@tensor.ru>
2023-11-02 13:42:15 +0500

    * добавлена модель Post

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+POST_STATUSES = (
+    (0, "Draft"),
+    (1, "Publish")
+)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.IntegerField(choices=POST_STATUSES, default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
